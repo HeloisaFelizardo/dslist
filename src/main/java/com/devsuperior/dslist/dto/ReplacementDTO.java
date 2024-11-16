d15b794e0ee2031b47da7805ebfc3e8cce853ae0
Heloisa Felizardo <helocmp@yahoo.com.br>
2024-11-15 21:51:06 -0300

    feat: Move games on list

---
+++ src/main/java/com/devsuperior/dslist/dto/ReplacementDTO.java
@@ -0,0 +1,22 @@
+package com.devsuperior.dslist.dto;
+
+public class ReplacementDTO {
+    private Integer fromPosition;
+    private Integer toPosition;
+
+    public Integer getFromPosition() {
+        return fromPosition;
+    }
+
+    public void setFromPosition(Integer fromPosition) {
+        this.fromPosition = fromPosition;
+    }
+
+    public Integer getToPosition() {
+        return toPosition;
+    }
+
+    public void setToPosition(Integer toPosition) {
+        this.toPosition = toPosition;
+    }
+}
--- src/main/java/com/devsuperior/dslist/services/GameListService.java
+++ src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -5,6 +5,7 @@ import com.devsuperior.dslist.dto.GameListDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.entities.GameList;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repository.GameListRepository;
 import com.devsuperior.dslist.repository.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,9 +20,28 @@ public class GameListService {
     @Autowired
     private GameListRepository gameListRepository;
 
+    @Autowired
+    private GameRepository gameRepository;
+
     @Transactional(readOnly = true)
     public List<GameListDTO> findAll() {
         List<GameList> result = gameListRepository.findAll();
         return result.stream().map(GameListDTO::new).toList();
+    }
+
+    @Transactional
+    public void move(Long listId, int fromPosition, int toPosition) {
+
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+
+        GameMinProjection game = list.remove(fromPosition);
+        list.add(toPosition, game);
+
+        int min = Math.min(fromPosition, toPosition);
+        int max = Math.max(fromPosition, toPosition);
+
+        for (int i = min; i <= max; i++) {
+            gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+        }
     }
 }
